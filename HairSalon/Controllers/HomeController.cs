492077a0a9bd6454a1397fae9bf1471e3919587f
Alex Adamovic <alexanderadamovic@gmail.com>
2022-03-11 14:39:37 -0800

    add search clients function to splash page

--- HairSalon/Controllers/HomeController.cs
+++ HairSalon/Controllers/HomeController.cs
@@ -1,15 +1,46 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace HairSalon.Controllers
 {
-    public class HomeController : Controller
+  public class HomeController : Controller
+  {
+    private readonly HairSalonContext _clientsDb;
+
+    public HomeController(HairSalonContext db)
     {
-
-      [HttpGet("/")]
-      public ActionResult Index()
-      {
-        return View();
-      }
-
+      _clientsDb = db;
     }
+
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      return View();
+    }
+
+    public ActionResult SearchFail()
+    {
+      return View();
+    }
+
+    [HttpPost, ActionName("Search")]
+    public ActionResult Search(string search)
+    {
+      var thisClient = _clientsDb.Clients.FirstOrDefault(client => String.Equals(client.Name, search));
+
+      if (thisClient != null)
+      {
+        return RedirectToAction("Details", "Clients", new { id = thisClient.ClientId } );
+      }
+      else
+      {
+        // Search unsuccessful return to failure page
+        return RedirectToAction("Index");
+      }
+    }
+  }
 }
